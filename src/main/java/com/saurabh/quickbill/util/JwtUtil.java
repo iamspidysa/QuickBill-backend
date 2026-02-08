1d5149125422064e1e9c6751fc891411dc459a94
iamspidysa <iamspidysa@users.noreply.github.com>
2026-02-08 23:32:39 +0530

    upgrade jwt 0.9.1 to 0.11.5

--- src/main/java/com/saurabh/quickbill/util/JwtUtil.java
+++ src/main/java/com/saurabh/quickbill/util/JwtUtil.java
@@ -3,10 +3,13 @@ package com.saurabh.quickbill.util;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 
+import javax.crypto.SecretKey;
+import java.nio.charset.StandardCharsets;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
@@ -16,6 +19,10 @@ import java.util.function.Function;
 public class JwtUtil {
     @Value("${jwt.secret.key}")
     private String SECRET_KEY;
+
+    private SecretKey getSignKey(){
+        return Keys.hmacShaKeyFor(SECRET_KEY.getBytes(StandardCharsets.UTF_8));
+    }
 
     public String generateToken(UserDetails userDetails){
         Map<String,Object> claims = new HashMap<>();
@@ -28,7 +35,7 @@ public class JwtUtil {
                 .setSubject(subject)
                 .setIssuedAt(new Date(System.currentTimeMillis()))
                 .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 10))
-                .signWith(SignatureAlgorithm.HS256,SECRET_KEY)
+                .signWith(getSignKey(), SignatureAlgorithm.HS256)
                 .compact();
     }
 
@@ -45,9 +52,11 @@ public class JwtUtil {
         return claimsResolver.apply(claims);
     }
 
+//    Little code changed due to upgrade in JWT version.
     private Claims extractAllClaims(String token){
-        return Jwts.parser()
-                .setSigningKey(SECRET_KEY)
+        return Jwts.parserBuilder()
+                .setSigningKey(getSignKey())
+                .build()
                 .parseClaimsJws(token)
                 .getBody();
     }
